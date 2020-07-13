e2d86a6f751689e18a4f26c9d68c4d110648ac5d
Juan Manuel Jaramillo <jpanku@gmail.com>
2020-07-13 10:26:39 -0500

    Search endpoint implementation plus some minor refactor/changes.

--- src/main/scala/com/app/sourcing/service/UserService.scala
+++ src/main/scala/com/app/sourcing/service/UserService.scala
@@ -1,9 +1,9 @@
 package com.app.sourcing.service
 
-import com.app.sourcing.entity.{GitHubFullUser, GitHubUser}
+import com.app.sourcing.entity.{GitHubFullRepo, GitHubFullUser, GitHubUser}
 
 trait UserService {
   def getUsers(maxRequests: Int, initVal: Long): List[GitHubUser]
   def getUser(users: List[String]): List[Option[GitHubFullUser]]
-  def getBatchUser(init: Long, users: List[GitHubUser], maxRequests: Int): List[GitHubUser]
+  def searchUsers(maxRequests: Int, initPage: Long): List[GitHubUser]
 }
